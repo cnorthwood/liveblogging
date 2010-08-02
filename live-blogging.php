b439256b55d445aaa71989c0eb90262a0f98adcb
Chris Northwood <chris@pling.org.uk>
2010-08-02 23:54:52 +0000

    Add POT Just need to write legacy importer now, then ready for beta
    git-svn-id: http://plugins.svn.wordpress.org/live-blogging/trunk@270919 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- live-blogging.php
+++ live-blogging.php
@@ -3,28 +3,29 @@
 Plugin Name: Live Blogging
 Plugin URI: http://wordpress.org/extend/plugins/live-blogging/
 Description: Plugin to support automatic live blogging
-Version: 2.0
+Version: 1.9
 Author: Chris Northwood
 Author URI: http://www.pling.org.uk/
+Text-Domain: live-blogging
 */
 
 /*
-    Live Blogging for WordPress
-    Copyright (C) 2010 Chris Northwood <chris@pling.org.uk>
+Live Blogging for WordPress
+Copyright (C) 2010 Chris Northwood <chris@pling.org.uk>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /*
